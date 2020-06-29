01f0f4c66c44f9a78dac4037918891c75e0ad131
Dylan Dewey <dylandewey41@gmail.com>
2020-06-29 16:35:24 -0600

    adding city to local storage and into html

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 let currentLoc;
 let savedLocation = [];
 let APIKey = "f37afb30faeaad6b445e174ebe59ab37";
-let queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + APIKey;
+
 
 
 function initialize() {
@@ -12,4 +12,27 @@ function initialize() {
             currentLoc = savedLocation[savedLocation.length - 1];
             
         }
+}
+
+function error () {
+    currentLoc = "Woodland Hills"
+    getCurrent(currentLoc);
+}
+
+function getCurrent(city) {
+    let queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+        error: function () {
+            savedLocation.splice(savedLocation.indexOf(city), 1);
+            localStorage.setItem("weathercities", JSON.stringify(savedLocation));
+            initialize();
+        }
+    }).then(function (response)) {
+        let currCard = $('<div>').attr('class', 'card bg-light');
+        $('earthforecast').append(currCard);
+
+        let currCardHead = $
+    }
 }
