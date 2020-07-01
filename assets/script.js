b85dafa4e85a41b9f2f5fd30ae190aadfd9c2e08
dylandewey <dylandewey41@gmail.com>
2020-06-30 18:11:39 -0600

    debugging

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ let APIKey = "f37afb30faeaad6b445e174ebe59ab37";
 
 function initialize() {
     savedLocation = JSON.parse(localStorage.getItem("weathercities"));
-    let lastSearch;
+    let lastSearch
         if (savedLocation) {
             //display previous location
             currentLoc = savedLocation[savedLocation.length - 1];
@@ -23,13 +23,15 @@ function getCurrent(city) {
     let queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     $.ajax({
         url: queryURL,
-        method: 'GET'
+        method: 'GET',
         error() {
             savedLocation.splice(savedLocation.indexOf(city), 1);
             localStorage.setItem("weathercities", JSON.stringify(savedLocation));
             initialize();
+        
         }
-    }).then(function (response) {
+    })
+        .then(function (response) {
         let currCard = $('<div>').attr('class', 'card bg-light');
         $('earthforecast').append(currCard);
 
